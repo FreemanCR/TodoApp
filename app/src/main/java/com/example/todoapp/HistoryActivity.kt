7e3cfdb652dc84c7ec02978ec2d91cb727f793c8
FreemanCR <renfer.111.meil.ru@gmail.com>
2022-11-03 21:37:01 +0200

    We add BackGroundGradint files, fix some bugs and change main all activity

--- app/src/main/java/com/example/todoapp/HistoryActivity.kt
+++ app/src/main/java/com/example/todoapp/HistoryActivity.kt
@@ -1,12 +1,25 @@
 package com.example.todoapp
 
+import android.graphics.drawable.AnimationDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.LinearLayout
+import kotlinx.android.synthetic.main.activity_history.*
 
 class HistoryActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_history)
+
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+
+        val animDrawable = historyLayout.background as AnimationDrawable
+        animDrawable.setEnterFadeDuration(3)
+        animDrawable.setExitFadeDuration(5000)
+        animDrawable.start()
+
+
     }
 }
